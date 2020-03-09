7c6cbdeeb15ea0d462b08e90c27c610211eed6df
Galder Zamarreño <galder@zamarreno.com>
2020-03-09 16:38:31 +0100

    Smaller dependency set

--- src/build.java
+++ src/build.java
@@ -124,9 +124,10 @@ class SequentialBuild
 
 class Mx
 {
+    private static final Logger LOG = LogManager.getLogger(OperatingSystem.class);
     private static final Pattern VERSION_PATTERN = Pattern.compile("\"([0-9]\\.[0-9]{1,3}\\.[0-9]{1,2})\"");
 
-    private static final String SVM_DEPENDENCIES = String.join(","
+    private static final String SVM_ONLY = String.join(","
         , "SVM"
         , "com.oracle.svm.graal"
         , "com.oracle.svm.truffle"
@@ -146,144 +147,55 @@ class Mx
         , "com.oracle.svm.reflect"
         , "com.oracle.svm.util" // svm.core dependency
         , "com.oracle.svm.core.graal" // svm.hosted dependency
-        // , "SVM_HOSTED_NATIVE" // -- do not even try to download native dependencies
-        // , "GRAAL_SDK" // -- previously built and available as mvn dependency
-        , "OBJECTFILE"
-        , "com.oracle.objectfile"
-        , "POINTSTO"
-        , "com.oracle.graal.pointsto"
-        // , "JUNIT_TOOL" // -- don't need test tools in production artifacts
+        // Explicit dependency to avoid pulling libffi
         , "TRUFFLE_NFI"
         , "com.oracle.truffle.nfi"
         , "com.oracle.truffle.nfi.spi"
-
-        , "TRUFFLE_API"
-        , "com.oracle.truffle.api"
-        , "com.oracle.truffle.api.dsl"
-        , "com.oracle.truffle.api.profiles"
-        , "com.oracle.truffle.api.debug"
-        , "com.oracle.truffle.api.utilities"
-        , "com.oracle.truffle.object"
-        , "com.oracle.truffle.api.object.dsl"
-        , "com.oracle.truffle.polyglot"
-        , "com.oracle.truffle.api.interop" // api.polyglot dependency
-        , "com.oracle.truffle.api.instrumentation" // api.polyglot dependency
-        , "com.oracle.truffle.api.utilities" // api.polyglot dependency
-        , "com.oracle.truffle.api.profiles" // api.interop dependency
-        , "com.oracle.truffle.api.library" // api.interop dependency
-        , "com.oracle.truffle.api.object" // truffle.object dependency
-
-        , "TRUFFLE_DSL_PROCESSOR"
-        , "truffle:ANTLR4"
-        , "com.oracle.truffle.dsl.processor"
-        , "com.oracle.truffle.object.dsl.processor"
-        , "com.oracle.truffle.dsl.processor.interop"
-
-        , "GRAAL"
-        , "org.graalvm.libgraal"
-        , "org.graalvm.compiler.options"
-        , "org.graalvm.compiler.nodeinfo"
-        , "org.graalvm.compiler.serviceprovider"
-        , "org.graalvm.compiler.api.replacements"
-        , "org.graalvm.compiler.api.runtime"
-        , "org.graalvm.compiler.graph"
-        , "org.graalvm.compiler.core"
-        , "org.graalvm.compiler.replacements"
-        , "org.graalvm.compiler.runtime"
-        , "org.graalvm.compiler.code"
-        , "org.graalvm.compiler.printer"
-        , "org.graalvm.compiler.core.aarch64"
-        , "org.graalvm.compiler.replacements.aarch64"
-        , "org.graalvm.compiler.core.amd64"
-        , "org.graalvm.compiler.replacements.amd64"
-        , "org.graalvm.compiler.core.sparc"
-        , "org.graalvm.compiler.replacements.sparc"
-        , "org.graalvm.compiler.hotspot.aarch64"
-        , "org.graalvm.compiler.hotspot.amd64"
-        , "org.graalvm.compiler.hotspot.sparc"
-        , "org.graalvm.compiler.hotspot"
-        , "org.graalvm.compiler.lir.aarch64"
-        , "org.graalvm.compiler.truffle.compiler.amd64"
-        , "org.graalvm.compiler.truffle.runtime.serviceprovider"
-        , "org.graalvm.compiler.truffle.runtime.hotspot"
-        , "org.graalvm.compiler.truffle.runtime.hotspot.java"
-        , "org.graalvm.compiler.truffle.runtime.hotspot.libgraal"
-        , "org.graalvm.compiler.truffle.compiler.hotspot.amd64"
-        , "org.graalvm.compiler.truffle.compiler.hotspot.sparc"
-        , "org.graalvm.compiler.truffle.compiler.hotspot.aarch64"
-        , "org.graalvm.compiler.api.directives" // compiler.replacements dependency
-        , "org.graalvm.compiler.java" // compiler.replacements dependency
-        , "org.graalvm.compiler.loop.phases" // compiler.replacements dependency
-        , "org.graalvm.compiler.word" // compiler.replacements dependency
-        , "org.graalvm.compiler.nodes" // compiler.word dependency
-        , "org.graalvm.compiler.lir" // compiler.nodes dependency
-        , "org.graalvm.compiler.asm" // compiler.lir dependency
-        , "org.graalvm.compiler.code" // compiler.lir dependency
-        , "org.graalvm.compiler.nodeinfo" // compiler.graph dependency
-        , "org.graalvm.compiler.core.common" // compiler.graph dependency
-        , "org.graalvm.compiler.bytecode" // compiler.graph dependency
-        , "org.graalvm.compiler.loop" // compiler.loop.phases dependency
-        , "org.graalvm.compiler.phases.common" // compiler.loop.phases dependency
-        , "org.graalvm.compiler.phases" // compiler.phases.common dependency
-        , "org.graalvm.compiler.lir" // compiler.lir.aarch64 dependency
-        , "org.graalvm.compiler.asm.aarch64" // compiler.lir.aarch64 dependency
-        , "org.graalvm.compiler.lir.amd64" // compiler.core.amd64 dependency
-        , "org.graalvm.compiler.asm.amd64" // compiler.lir.amd64 dependency
-        , "org.graalvm.compiler.lir.sparc" // compiler.hotspot.sparc dependency (TODO missing in mx.compiler/suite.py)
-        , "org.graalvm.compiler.truffle.compiler" // compiler.truffle.compiler.amd64 dependency
-        , "org.graalvm.compiler.truffle.runtime" // compiler.truffle.runtime.hotspot dependency
-        , "org.graalvm.compiler.truffle.common.hotspot" // compiler.truffle.runtime.hotspot dependency
-        , "org.graalvm.compiler.truffle.compiler.hotspot" // compiler.truffle.runtime.hotspot.java dependency
-        , "org.graalvm.compiler.truffle.common.hotspot.libgraal" // compiler.truffle.runtime.hotspot.libgraal dependency
-        , "org.graalvm.util" // compiler.truffle.runtime.hotspot.libgraal dependency
-        , "org.graalvm.compiler.debug" // compiler.core.common dependency
-        , "org.graalvm.compiler.virtual" // compiler.core dependency
-        , "org.graalvm.compiler.asm.sparc" // compiler.lir.sparc dependency
-
-        , "GRAAL_PROCESSOR_COMMON"
-        , "org.graalvm.compiler.processor"
-
-        , "GRAAL_OPTIONS_PROCESSOR"
-        , "org.graalvm.compiler.options.processor"
-
-        , "GRAAL_NODEINFO_PROCESSOR"
-        , "org.graalvm.compiler.nodeinfo.processor"
-
-        , "GRAAL_SERVICEPROVIDER_PROCESSOR"
-        , "org.graalvm.compiler.serviceprovider.processor"
-
-        , "GRAAL_REPLACEMENTS_PROCESSOR"
-        , "org.graalvm.compiler.replacements.processor"
-
-        , "GRAAL_COMPILER_MATCH_PROCESSOR"
-        , "org.graalvm.compiler.core.match.processor"
-
-        , "GRAAL_GRAPHIO"
-        , "org.graalvm.graphio"
-
-        , "GRAAL_TRUFFLE_COMMON"
-        , "org.graalvm.compiler.truffle.common"
-
-        , "TRUFFLE_COMMON_PROCESSOR"
-        , "org.graalvm.compiler.truffle.common.processor"
-
-        , "JVMCI_HOTSPOT"
-        , "JVMCI_API"
     );
 
-    static final Map<String, Stream<String>> BUILD_ARGS = Map.of(
-        "sdk", Stream.empty()
-        , "substratevm", Stream.of("--only", SVM_DEPENDENCIES)
+    static final Map<String, Stream<BuildArgs>> BUILD_STEPS = Map.of(
+        "sdk", Stream.of(BuildArgs.empty())
+        , "substratevm", Stream.of(
+            BuildArgs.of("--dependencies", "GRAAL")
+            , BuildArgs.of("--dependencies", "POINTSTO")
+            , BuildArgs.of("--dependencies", "OBJECTFILE")
+            , BuildArgs.of("--only", SVM_ONLY)
+        )
     );
 
     static Consumer<String> build(Options options)
     {
         return artifactName ->
-            OperatingSystem.exec()
-                .compose(Mx.mxbuild(options))
-                .compose(Mx.hookMavenProxy(options))
+        {
+            LOG.debugf("Build %s", artifactName);
+
+            final var artifact = Mx.hookMavenProxy(options)
                 .compose(Mx::artifact)
                 .apply(artifactName);
+
+            artifact.buildSteps
+                .map(Mx.mxbuild(artifact, options))
+                .forEach(OperatingSystem::exec);
+        };
+    }
+
+    private static Function<BuildArgs, OperatingSystem.Command> mxbuild(Artifact artifact, Options options)
+    {
+        return buildArgs ->
+            new OperatingSystem.Command(
+                Stream.concat(
+                    Stream.of(
+                        artifact.mxHome.resolve("mx").toString()
+                        , options.verbose ? "-V" : ""
+                        , "--trust-http"
+                        , "build"
+                        , "--no-native"
+                    )
+                    , buildArgs.args
+                )
+                , artifact.rootPath
+                , Stream.empty()
+            );
     }
 
     private static Artifact artifact(String artifactName)
@@ -294,7 +206,7 @@ class Mx
             .compose(LocalPaths.graalHome())
             .compose(Mx::suitePy)
             .apply(artifactName);
-        var buildArgs = BUILD_ARGS.get(artifactName);
+        var buildArgs = BUILD_STEPS.get(artifactName);
         return new Artifact(rootPath, mxHome, buildArgs);
     }
 
@@ -330,25 +242,6 @@ class Mx
             .findFirst()
             .map(result -> result.group(1))
             .orElse(null);
-    }
-
-    private static Function<Artifact, OperatingSystem.Command> mxbuild(Options options)
-    {
-        return artifact ->
-            new OperatingSystem.Command(
-                Stream.concat(
-                    Stream.of(
-                        artifact.mxHome.resolve("mx").toString()
-                        , options.verbose ? "-V" : ""
-                        , "--trust-http"
-                        , "build"
-                        , "--no-native"
-                    )
-                    , artifact.buildArgs
-                )
-                , artifact.rootPath
-                , Stream.empty()
-            );
     }
 
     static Function<Artifact, Artifact> hookMavenProxy(Options options)
@@ -430,21 +323,41 @@ class Mx
         }
     }
 
+    static class BuildArgs
+    {
+        final Stream<String> args;
+
+        private BuildArgs(Stream<String> args)
+        {
+            this.args = args;
+        }
+
+        static BuildArgs of(String... args)
+        {
+            return new BuildArgs(Stream.of(args));
+        }
+
+        static BuildArgs empty()
+        {
+            return new BuildArgs(Stream.empty());
+        }
+    }
+
     private static class Artifact
     {
         final Path rootPath;
         final Path mxHome;
-        final Stream<String> buildArgs;
+        final Stream<BuildArgs> buildSteps;
 
         Artifact(
             Path rootPath
             , Path mxHome
-            , Stream<String> buildArgs
+            , Stream<BuildArgs> buildSteps
         )
         {
             this.rootPath = rootPath;
             this.mxHome = mxHome;
-            this.buildArgs = buildArgs;
+            this.buildSteps = buildSteps;
         }
     }
 }
@@ -553,20 +466,24 @@ class Maven
 
 class OperatingSystem
 {
-    static final Logger logger = LogManager.getLogger(OperatingSystem.class);
+    static final Logger LOG = LogManager.getLogger(OperatingSystem.class);
 
     static Function<OperatingSystem.Command, Void> exec()
     {
-        return OperatingSystem::exec;
+        return command ->
+        {
+            exec(command);
+            return null;
+        };
     }
 
-    private static Void exec(Command command)
+    static void exec(Command command)
     {
         final var commandList = command.command
             .filter(Predicate.not(String::isEmpty))
             .collect(Collectors.toList());
 
-        logger.debugf("Execute %s in %s", commandList, command.directory);
+        LOG.debugf("Execute %s in %s", commandList, command.directory);
         try
         {
             var processBuilder = new ProcessBuilder(commandList)
@@ -586,8 +503,6 @@ class OperatingSystem
                     "Failed, exit code: " + process.exitValue()
                 );
             }
-
-            return null;
         }
         catch (Exception e)
         {
