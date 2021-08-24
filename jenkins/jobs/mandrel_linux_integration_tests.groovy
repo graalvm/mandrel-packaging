11b25ebe7606c127ad0173fe02cecf3456504e3a
Michal Karm Babacek <karm@redhat.com>
2021-08-24 17:32:19 +0200

    JDK17 jobs

--- jenkins/jobs/mandrel_linux_integration_tests.groovy
+++ jenkins/jobs/mandrel_linux_integration_tests.groovy
@@ -4,12 +4,13 @@ matrixJob('mandrel-linux-integration-tests') {
                 'graal-vm-20.3',
                 '20.3',
                 '21.2',
-                'master'
+                'master',
+                'master-jdk17'
         )
         text('QUARKUS_VERSION',
                 '1.11.7.Final',
-                '2.1.2.Final',
-                '2.1.0.CR1'
+                '2.1.3.Final',
+                '2.2.0.CR1'
         )
         labelExpression('LABEL', ['el8'])
     }
@@ -28,7 +29,7 @@ matrixJob('mandrel-linux-integration-tests') {
     combinationFilter(
             ' (MANDREL_VERSION.contains("20.3") && QUARKUS_VERSION.startsWith("1.")) ||' +
             ' (MANDREL_VERSION.contains("20.3") && QUARKUS_VERSION.startsWith("2.")) ||' +
-            ' ((MANDREL_VERSION.contains("21.2") || MANDREL_VERSION=="master") && QUARKUS_VERSION.startsWith("2."))')
+            ' ((MANDREL_VERSION.contains("21.2") || MANDREL_VERSION.contains("master")) && QUARKUS_VERSION.startsWith("2."))')
     parameters {
         stringParam('MANDREL_INTEGRATION_TESTS_REPO', 'https://github.com/Karm/mandrel-integration-tests.git', 'Test suite repository.')
         choiceParam(
@@ -56,6 +57,7 @@ matrixJob('mandrel-linux-integration-tests') {
                 20.3)           BUILD_JOB='mandrel-20.3-linux-build';;
                 21.2)           BUILD_JOB='mandrel-21.2-linux-build';;
                 master)         BUILD_JOB='mandrel-master-linux-build';;
+                master-jdk17)   BUILD_JOB='mandrel-master-jdk17-linux-build';;
                 *)
                     echo "UNKNOWN Mandrel version: $MANDREL_VERSION"
                     exit 1
