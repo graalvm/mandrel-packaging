276940d0be19967c97bdade4ca14af087de35290
Michal Karm Babacek <karm@redhat.com>
2021-08-26 20:46:03 +0200

    JDK17 Windows support

--- jenkins/jobs/mandrel_windows_integration_tests.groovy
+++ jenkins/jobs/mandrel_windows_integration_tests.groovy
@@ -4,7 +4,8 @@ matrixJob('mandrel-windows-integration-tests') {
                 'graal-vm-20.3',
                 '20.3',
                 '21.2',
-                'master'
+                'master',
+                'master-jdk17'
         )
         text('QUARKUS_VERSION',
                 '1.11.7.Final',
@@ -28,7 +29,7 @@ matrixJob('mandrel-windows-integration-tests') {
     combinationFilter(
             ' (MANDREL_VERSION.contains("20.3") && QUARKUS_VERSION.startsWith("1.")) ||' +
             ' (MANDREL_VERSION.contains("20.3") && QUARKUS_VERSION.startsWith("2.")) ||' +
-            ' ((MANDREL_VERSION.contains("21.2") || MANDREL_VERSION=="master") && QUARKUS_VERSION.startsWith("2."))')
+            ' ((MANDREL_VERSION.contains("21.2") || MANDREL_VERSION.contains("master")) && QUARKUS_VERSION.startsWith("2."))')
     parameters {
         stringParam('MANDREL_INTEGRATION_TESTS_REPO', 'https://github.com/Karm/mandrel-integration-tests.git', 'Test suite repository.')
         choiceParam(
@@ -63,6 +64,8 @@ IF "%MANDREL_VERSION%"=="20.3" (
     set BUILD_JOB=mandrel-21.2-windows-build
 ) ELSE IF "%MANDREL_VERSION%"=="master" (
     set BUILD_JOB=mandrel-master-windows-build
+) ELSE IF "%MANDREL_VERSION%"=="master-jdk17" (
+    set BUILD_JOB=mandrel-master-jdk17-windows-build
 ) ELSE (
     echo "UNKNOWN Mandrel version: %MANDREL_VERSION%"
     exit 1
