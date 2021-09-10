be0e2b243c956f1d825eb0eb6ee00d35592085fe
Michal Karm Babacek <karm@redhat.com>
2021-09-10 07:11:14 +0200

    QUarkus 2.2.2.Final

--- jenkins/jobs/mandrel_windows_integration_tests.groovy
+++ jenkins/jobs/mandrel_windows_integration_tests.groovy
@@ -10,7 +10,7 @@ matrixJob('mandrel-windows-integration-tests') {
         text('QUARKUS_VERSION',
                 '1.11.7.Final',
                 '2.1.3.Final',
-                '2.2.0.Final'
+                '2.2.2.Final'
         )
         labelExpression('LABEL', ['w2k19'])
     }
--- jenkins/jobs/mandrel_windows_quarkus_tests.groovy
+++ jenkins/jobs/mandrel_windows_quarkus_tests.groovy
@@ -9,7 +9,7 @@ matrixJob('mandrel-windows-quarkus-tests') {
         )
         text('QUARKUS_VERSION',
                 '1.11.7.Final',
-                '2.2.0.Final',
+                '2.2.2.Final',
                 'main'
         )
         labelExpression('LABEL', ['w2k19&&docker'])
