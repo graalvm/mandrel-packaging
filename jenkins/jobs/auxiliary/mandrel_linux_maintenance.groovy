c76bd1846dc18e435243a88e49b5fe84a6be851a
Michal Karm Babacek <karm@redhat.com>
2022-07-07 12:07:02 +0200

    Some cleanup for Linux executors, Q exclusions

---
+++ jenkins/jobs/auxiliary/mandrel_linux_maintenance.groovy
@@ -0,0 +1,41 @@
+package jenkins.jobs.auxiliary
+
+final Class Constants = new GroovyClassLoader(getClass().getClassLoader())
+        .parseClass(readFileFromWorkspace("jenkins/jobs/builds/Constants.groovy"))
+matrixJob('mandrel-linux-maintenance') {
+    axes {
+        labelExpression('LABEL', [
+            'karm-rhel8-x86-64',
+            'rhel8-aarch64-vm-1',
+            'rhel8-aarch64-vm-2',
+            'rhel8-upstream-1',
+            'rhel8-upstream-2',
+            'rhel8-upstream-3',
+            'rhel8-upstream-4'
+        ])
+    }
+    logRotator {
+        numToKeep(5)
+    }
+    triggers {
+        cron {
+            spec('H H * * *')
+        }
+    }
+    steps {
+        shell {
+            command('''
+            set +e
+            sudo podman stop $(sudo podman ps -a -q)
+            sudo podman rm $(sudo podman ps -a -q)
+            yes | sudo podman system prune
+            podman stop $(podman ps -a -q)
+            podman rm $(podman ps -a -q)
+            yes | podman system prune
+            set -e
+            sudo shutdown -r now
+            ''')
+            unstableReturn(1)
+        }
+    }
+}
