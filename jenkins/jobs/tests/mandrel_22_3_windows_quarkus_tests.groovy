351ce1d7f2d5b7c059c536cb00ea6383cffa94fd
Michal Karm Babacek <karm@redhat.com>
2022-11-01 00:34:30 +0100

    Job DSL: Adds jobs to debug longer Q TS runs

---
+++ jenkins/jobs/tests/mandrel_22_3_windows_quarkus_tests.groovy
@@ -0,0 +1,77 @@
+package jenkins.jobs.tests
+
+final Class Constants = new GroovyClassLoader(getClass().getClassLoader())
+        .parseClass(readFileFromWorkspace("jenkins/jobs/tests/Constants.groovy"))
+matrixJob('mandrel-22-3-windows-quarkus-tests') {
+    axes {
+        text('JDK_VERSION',
+                '17'
+        )
+        text('JDK_RELEASE',
+                'ga'
+        )
+        text('MANDREL_BUILD',
+                'mandrel-22-3-windows-build-matrix'
+        )
+        text('QUARKUS_VERSION', Constants.QUARKUS_VERSION_SHORT)
+        labelExpression('LABEL', ['w2k19'])
+    }
+    description('Run Quarkus TS with Mandrel distros. Quarkus versions differ according to particular Mandrel versions.')
+    displayName('Windows :: Quarkus TS :: 22.3')
+    logRotator {
+        numToKeep(30)
+    }
+    childCustomWorkspace('${SHORT_COMBINATION}')
+    wrappers {
+        preBuildCleanup()
+        timestamps()
+        timeout {
+            absolute(820)
+        }
+    }
+    combinationFilter(
+            '!(QUARKUS_VERSION.equals("main") && (MANDREL_BUILD.contains("mandrel-21") || JDK_VERSION.contains("11")))'
+    )
+    parameters {
+        stringParam('QUARKUS_REPO', 'https://github.com/quarkusio/quarkus.git', 'Quarkus repository.')
+        stringParam('QUARKUS_MODULES', Constants.QUARKUS_MODULES_TESTS)
+        stringParam(
+                'MANDREL_BUILD_NUMBER',
+                'lastSuccessfulBuild',
+                'Pick a build number from MANDREL_BUILD or leave the default latest.'
+        )
+    }
+    steps {
+        batchFile {
+            command(Constants.WINDOWS_QUARKUS_TESTS)
+            unstableReturn(1)
+        }
+    }
+    publishers {
+        archiveJunit('**/target/*-reports/*.xml') {
+            allowEmptyResults(false)
+            retainLongStdout(false)
+            healthScaleFactor(1.0)
+        }
+        archiveArtifacts('**/target/*-reports/*.xml')
+        extendedEmail {
+            recipientList('karm@redhat.com')
+            triggers {
+                failure {
+                    sendTo {
+                        recipientList()
+                        attachBuildLog(true)
+                    }
+                }
+            }
+        }
+        wsCleanup()
+        postBuildCleanup {
+            cleaner {
+                psCleaner {
+                    killerType('org.jenkinsci.plugins.proccleaner.PsRecursiveKiller')
+                }
+            }
+        }
+    }
+}
