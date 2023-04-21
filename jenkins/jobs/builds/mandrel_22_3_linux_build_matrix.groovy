9a147786c210b86c40bceeafa5ff0316410ff825
Michal Karm Babacek <karm@redhat.com>
2023-04-21 14:18:20 +0200

    JDK 20 added for Mandrel 22+

--- jenkins/jobs/builds/mandrel_22_3_linux_build_matrix.groovy
+++ jenkins/jobs/builds/mandrel_22_3_linux_build_matrix.groovy
@@ -7,7 +7,8 @@ matrixJob('mandrel-22-3-linux-build-matrix') {
         labelExpression('LABEL', ['el8_aarch64', 'el8'])
         text('JDK_VERSION',
                 '11',
-                '17'
+                '17',
+                '20'
         )
         text('JDK_RELEASE',
                 'ea',
