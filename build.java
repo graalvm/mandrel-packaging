404a54ad5a01a375453e900549361a1193febe38
Severin Gehwolf <sgehwolf@redhat.com>
2024-02-13 12:03:19 +0100

    Add SVM_FOREIGN to maven deploy steps

--- build.java
+++ build.java
@@ -996,6 +996,7 @@ class Mx
                 "SVM_AGENT," +
                 "SVM_DIAGNOSTICS_AGENT," +
                 "SVM_CONFIGURE," +
+                "SVM_FOREIGN," +
                 "MANDREL_PACKAGING_WRAPPER")
     );
 
